dc908445e01df691f1ae40560001b45e911b2574
AugustinMorel <augustin.morel@telecom-paristech.fr>
2016-04-19 22:20:16 +0200

    Ajout d'un fichier a propos

---
+++ view/a_propos.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>BUS PLANNER</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Bootstrap theme -->
+    <link href="css/bootstrap-theme.min.css" rel="stylesheet">
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="main.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="js/ie-emulation-modes-warning.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body role="document">
+
+
+
+
+    <!-- Barre de navigation -->
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Telecom Bus Planner</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Accueil</a></li>
+            <li><a href="#apropos">A propos</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+
+    <div class="container theme-showcase" role="main">
+
+
+
+      <!-- En-tête -->
+      <div class="jumbotron">
+        <h1>Telecom Bus Planner</h1>
+        <p>Bienvenue sur Telecom Bus Planner. Ici tu vas pouvoir prévoir en toute sérénité tes trajets en bus pour les différents week-end organisés par ton BDE préféré. Si tu souhaites partager le voyage avec tous tes amis, n'hésite pas et connecte toi dès maintenant.</p>
+      </div>
+
+
+      <!-- A propos -->
+
+      <div class="page-header">
+        <h1>A propos</h1>
+      </div>
+
+
+
+      <div class="row">
+
+        <div class="col-sm-4">
+          <div class="panel panel-primary">
+            <div class="panel-heading">
+              <h3 class="panel-title">Le projet</h3>
+            </div>
+            <div class="panel-body">
+                <div style="text-align:justify" >
+                  Ce site a été réalisé dans le cadre du cours d'Interface Homme-Machine (IGR 203) de Telecom ParisTech. Le but était après avoir trouvé une idée de projet viable,
+                  de définir les besoins des utilisateurs et de les traduire dans une application fonctionnelle. Etant donné les fonctionnalités offertes par cette application nous avons immédiatement
+                  opté pour un site WEB.
+                </div>
+            </div>
+          </div>
+        </div><!-- /.col-sm-4 -->
+
+        <div class="col-sm-4">
+          <div class="panel panel-primary">
+            <div class="panel-heading">
+              <h3 class="panel-title">Origine</h3>
+            </div>
+            <div class="panel-body">
+            <div style="text-align:justify">
+              Tout a commencé lors d'un retour du WES (Week-end ski) où Martin et Bernardo ont passé à peu près 20 minutes à trouver de la place dans le bus. L'organisation souvent "au
+              dernier moment" du BDE n'aidant pas, il n'est jamais aisé de s'y retrouver dans le bus, notamment quand on veut rentrer à la CiuP. C'est alors que Bernardo a été frappé par un
+              éclair de génie : on peut prévoir tout cela à l'avance ! C'est le rôle de Telecom Bus Planner.
+            </div>
+            </div>
+          </div>
+        </div><!-- /.col-sm-4 -->
+
+        <div class="col-sm-4">
+          <div class="panel panel-primary">
+            <div class="panel-heading">
+              <h3 class="panel-title">Equipe de développement</h3>
+            </div>
+            <div class="panel-body">
+              Benjamin Auclair <br></br> Martin Chochod <br></br> Bernardo Cordeiro <br></br> Augustin Morel
+            </div>
+          </div>
+        </div><!-- /.col-sm-4 -->
+
+      </div>
