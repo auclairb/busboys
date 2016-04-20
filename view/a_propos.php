a4ec41aeb9002b482bf58bc6983637fed7e2a021
AugustinMorel <augustin.morel@telecom-paristech.fr>
2016-04-20 13:20:07 +0200

    Nouvel a propos et gestion des images

--- view/a_propos.php
+++ view/a_propos.php
@@ -78,24 +78,53 @@
       </div>
 
       <h3>Le projet</h3>
-      <p>Ce site a été réalisé dans le cadre du cours d'Interface Homme-Machine (IGR 203) de Telecom ParisTech. Le but était après avoir trouvé une idée de projet viable,
-      de définir les besoins des utilisateurs et de les traduire dans une application fonctionnelle. Etant donné les fonctionnalités offertes par cette application nous avons immédiatement
+      <p>Ce site a été réalisé dans le cadre du cours d'Interface Homme-Machine (IGR 203) de Telecom ParisTech. Le but était, après avoir trouvé une idée de projet viable,
+      de définir les besoins des utilisateurs et de les traduire dans une application fonctionnelle. Etant données les fonctionnalités offertes par cette application nous avons immédiatement
       opté pour un site WEB.</p>
 
       <h3>Origine</h3>
-      <p>Tout a commencé lors d'un retour du WES (Week-end ski) où Martin et Bernardo ont passé à peu près 20 minutes à trouver de la place dans le bus. L'organisation souvent "au
-      dernier moment" du BDE n'aidant pas, il n'est jamais aisé de s'y retrouver dans le bus, notamment quand on veut rentrer à la CiuP. C'est alors que Bernardo a été frappé par un
+      <p>Tout a commencé lors d'un retour du WES (Week-end ski) où Martin et Bernardo ont passé à peu près 20 minutes à trouver de la place dans un bus. L'organisation souvent "au
+      dernier moment" du BDE n'aidant pas, il n'est jamais aisé de s'y retrouver dans les bus, notamment quand on veut rentrer à la CiuP. C'est alors que Bernardo a été frappé par un
       éclair de génie : on peut prévoir tout cela à l'avance ! C'est le rôle de Telecom Bus Planner.</p>
 
       <h3>L'équipe</h3>
-      <p>L'équipe de développement est constituée à 100% d'élèves de l'école de tous horizons : de la Bretagne au Pays-Bas en passant par le Brésil ou encore les Haut de Seine il y en a pour tous les goûts ! </p>
+      <p>L'équipe de développement est constituée à 100% d'élèves de l'école de tous horizons : de la Bretagne au Pays-Bas en passant par le Brésil ou encore les Hauts de Seine il y en a pour tous les goûts ! </p>
 
-      <h3>Constitution</h3>
-      <div class="row">
-        <div class="col-xs-3">Benjamin AUCLAIR</div>
-        <div class="col-xs-3">Martin CHOCHOD</div>
-        <div class="col-xs-3">Bernardo CORDEIRO</div>
-        <div class="col-xs-3">Augustin MOREL</div>
+
+
+              <div class="row">
+                <div class="col-xs-3">
+                <center>
+                <p><IMG src="picture/benjamin.png" alt="Benjamin" border="0" width="200" height="200"></p>
+                <p><strong>Benjamin AUCLAIR</strong></p>
+                </center>
+              </div>
+
+              <div class="row">
+                <div class="col-xs-3">
+                <center>
+                <p><IMG src="picture/martin.png" alt="Martin" border="0" width="200" height="200"></p>
+                <p><strong>Martin CHOCHOD</strong></p>
+                </center>
+              </div>
+
+              <div class="row">
+                <div class="col-xs-3">
+                <center>
+                <p><IMG src="picture/bernardo.png" alt="Bernaro" border="0" width="200" height="200"></p>
+                <p><strong>Bernardo CORDEIRO</strong></p>
+                </center>
+              </div>
+
+              <div class="row">
+                <div class="col-xs-3">
+                <center>
+                <p><IMG src="picture/Augustin.png" alt="Augustin" border="0" width="200" height="200"></p>
+                <p><strong>Augustin MOREL</strong></p>
+                </center>
+              </div>
+
+
       </div>
 
     </div> <!-- /container -->
